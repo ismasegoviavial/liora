f0b59b0875a5699e5257026ac0d5aa032ed53921
Isma Segovia <ismasegoviavial@gmail.com>
2026-08-26 15:25:17 -0400

    fix: Set default DATABASE_URL file:/tmp/dev.db in prisma.ts

--- personal-finance-saas/src/lib/prisma.ts
+++ personal-finance-saas/src/lib/prisma.ts
@@ -1,5 +1,9 @@
 import { PrismaClient } from "@prisma/client"
 
+if (!process.env.DATABASE_URL) {
+  process.env.DATABASE_URL = "file:/tmp/dev.db"
+}
+
 const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
 let prisma: PrismaClient
